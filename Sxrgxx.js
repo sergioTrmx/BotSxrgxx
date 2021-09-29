89cb922c521936a3126bb2ce8263eac07a31dd5e
sergioTrmx <80510599+sergioTrmx@users.noreply.github.com>
2021-09-28 23:13:32 -0600

    Update Sxrgxx.js

--- Sxrgxx.js
+++ Sxrgxx.js
@@ -9,7 +9,7 @@ async function iniciar () {
         client.on('qr', () => {
         })
 
-        fs.existsSync('./Samu330.json') && client.loadAuthInfo('./Samu330.json')
+        fs.existsSync('./sxrgxx.json') && client.loadAuthInfo('./sxrgxx.json')
 
         client.on('connecting', () => {
         console.log('Conectando')
